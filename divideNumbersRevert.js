cc66f97dfc49df12a834244012d59f11f252207f
Richard Aspinall <rls.aspinall@gmail.com>
2024-09-17 09:21:16 +1000

    Revert "Merge pull request #2 from richardaspinall/make-breaking-change"
    This reverts commit 1b11700756b03ff469da2759f9da84791d8a8d99, reversing
    changes made to df3ec10e86b5d359557e9977cae332d24fb7a704.

--- divideNumbersRevert.js
+++ divideNumbersRevert.js
@@ -3,10 +3,10 @@ function divideNumbers(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('Both arguments must be numbers');
   }
-  if ((b = 0)) {
+  if (b === 0) {
     throw new Error('Cannot divide by zero');
   }
-  return "I'm breaking the code!";
+  return a / b;
 }
 
 module.exports = divideNumbers;
